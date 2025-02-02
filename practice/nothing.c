609487157388e661ea63e5ca18263de5d4fd9df4
vanshverma2006 <vansh.verma18082006@gmail.com>
2025-02-02 17:50:14 +0530

    min and max element in array

--- practice/nothing.c
+++ practice/nothing.c
@@ -84,6 +84,36 @@ int main() {
     // }
     // printf("%d",n);
     
+    int n;
+    printf("enter a num :: ");
+    scanf("%d",&n);
+    int array[n][n];
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            printf("enter value of array[%d][%d] : ",i,j);
+            scanf("%d",&array[i][j]);
+        }
+    }
+    int largest,least ;
+    largest = array[0][0];
+    least=array[0][0];
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(array[i][j]>largest){
+                largest=array[i][j];
+            }
+        }
+    }
+
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(array[i][j]<least){
+                least=array[i][j];
+            }
+        }
+    }
+    printf("%d",largest);
+    printf("\n%d",least);
 
 
 
