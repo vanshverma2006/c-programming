60911ce09164c4c64d59a8309d2d07b5abe6311f
vanshverma2006 <vansh.verma18082006@gmail.com>
2025-04-09 12:26:53 +0530

    fuck you

--- practice/a.c
+++ practice/a.c
@@ -5,25 +5,40 @@ void inputArray(int array[],int n){
         scanf("%d",&array[i]);
     }
 }
+void printArray(int array[],int n){
+    for (int i=0;i<n;i++){
+        printf("%d ",array[i]);
+    }
+}
+void reverseArray(int array[],int n){
+
+    inputArray(array,n);
+    int temp[n];
+    for (int i = n-1 ;i>=0;i--){
+        temp[n-i-1]=array[i];
+    }
+    for (int i=0;i<n;i++){
+        array[i]=temp[i];
+    }
+    
+}
+void kShift(int array[],int n,int k){
+    inputArray(array,n);
+    int temp = n-k;
+    int newArray[temp];
+    for (int i=0 ;i<temp; i++){
+        newArray[i]= array[i];
+    }
+
+    
+}
 int main(){
     int n;
     scanf("%d",&n);
     int array[n];
-    // for (int i=0;i<n;i++){
-    //     scanf("%d",&array[i]);
-    // }
-    // int * ptr = array;
-    // for (int i=0 ;i<n;i++){
-    //     printf("%d ",*(ptr + i ));
-    // }
-    inputArray(array,n);
-    int *ptr = array;
-    for (int i=0;i<n;i++){
-        printf(" value %d ",*(ptr+i));
-    }
-    for (int i=0;i<n;i++){
-        printf(" address %p ",(ptr+i));
-    }
+    
+    reverseArray(array,n);
+    
 
     
     return 0;
